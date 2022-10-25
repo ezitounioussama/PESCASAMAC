db11a24454ecc65cc4912f6acfe0f79307322093
ezitounioussama <zitounioussama95@gmail.com>
2022-10-25 17:07:19 +0100

    70% done !

---
+++ chair_araignee/product.php
@@ -0,0 +1,170 @@
+<?php
+require_once('../inc/header.php');
+require_once('../inc/nav.php');
+require_once('../inc/db.php');
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM chairAraignee WHERE id = '$id'";
+$featured = mysqli_query($conn, $sql);
+
+?>
+<script src="../chair_araignee/assests/traitments.js" defer></script>
+<section>
+  <div class="relative mx-auto max-w-screen-xl px-4 py-8">
+    <div class="grid grid-cols-1 items-start gap-8 md:grid-cols-2">
+      <?php while ($product = mysqli_fetch_assoc($featured)) : ?>
+        <span class="hidden" id="id"><?= $product['id']; ?></span>
+        <div class="grid grid-cols-2 gap-4 md:grid-cols-1">
+          <img alt="<?= $product['name']; ?>" src="img/<?= $product['pic']; ?>" class="aspect-square w-full rounded-xl object-cover" id="myIMG" />
+          <!-- <div class="grid grid-cols-2 gap-4 lg:mt-4">
+            <img alt="Les Paul" src="../img/second.jpg" class="aspect-square w-full rounded-xl object-cover" id="altIMG" />
+          </div> -->
+
+
+
+
+
+        </div>
+
+        <div class="sticky top-0">
+          <!-- <strong class="rounded-full border border-blue-600 bg-gray-100 px-3 py-0.5 text-xs font-medium tracking-wide text-blue-600">
+            ******
+          </strong> -->
+
+          <div class="mt-8 flex justify-left">
+            <div class="max-w-[55ch]">
+              <h1 class="text-2xl font-bold"><?= $product['name']; ?></h1>
+            </div>
+          </div>
+
+          <!-- <details class="group relative mt-4">
+              <summary class="block">
+                <div>
+                  <div class="prose max-w-none group-open:hidden">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsa veniam dicta beatae eos ex error culpa delectus rem
+                      tenetur, architecto quam nesciunt, dolor veritatis nisi
+                      minus inventore, rerum at recusandae?
+                    </p>
+                  </div>
+
+                  <span
+                    class="mt-4 cursor-pointer text-sm font-medium underline group-open:absolute group-open:bottom-0 group-open:left-0 group-open:mt-0"
+                  >
+                    Read More
+                  </span>
+                </div>
+              </summary>
+
+              <div class="prose max-w-none pb-6">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+                  veniam dicta beatae eos ex error culpa delectus rem tenetur,
+                  architecto quam nesciunt, dolor veritatis nisi minus
+                  inventore, rerum at recusandae?
+                </p>
+
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Placeat nam sapiente nobis ea veritatis error consequatur nisi
+                  exercitationem iure laudantium culpa, animi temporibus non!
+                  Maxime et quisquam amet. A, deserunt!
+                </p>
+              </div>
+            </details> -->
+
+          <form class="mt-8">
+            <fieldset <?php if (!$product['c1'] && !$product['c2']) {
+                        echo "hidden";
+                      } ?>>
+              <legend class="mb-3 text-md font-medium">Calibre :</legend>
+
+              <div class="flow-root">
+
+                <div class="-m-0.5 flex flex-wrap justify-right">
+
+                  <select id="mycalibre" class="relative w-full rounded-lg  p-2.5 text-sm font-normal focus:z-10" id="country" name="country" autocomplete="country-name">
+
+                    <option value="" selected>Choisir une option</option>
+
+                    <option value="2" <?php if (!($product['c1'])) {
+                                        echo "hidden";
+                                      } ?>><?= $product['c1']; ?></option>
+                    <option value="3" <?php if (!($product['c2'])) {
+                                        echo "hidden";
+                                      } ?>><?= $product['c2']; ?></option>
+
+
+                  </select>
+
+
+                </div>
+              </div>
+            </fieldset>
+
+            <fieldset class="mt-8" <?php if (!$product['t1'] && !$product['t2']) {
+                                      echo "hidden";
+                                    } ?>>
+              <legend class="mb-3 text-md font-medium">
+                Choisissez le traitement :
+              </legend>
+
+              <div class="flow-root">
+                <div class="-m-0.5 flex flex-wrap">
+                  <select id="myselection" class="relative w-full rounded-lg  p-2.5 text-sm focus:z-10">
+                    <option value="" selected>Choisir une option</option>
+                    <option value="2" <?php if (!($product['t1'])) {
+                                        echo "hidden";
+                                      } ?>><?= $product['t1']; ?></option>
+                    <option value="3" <?php if (!($product['t2'])) {
+                                        echo "hidden";
+                                      } ?>><?= $product['t2']; ?></option>
+
+                  </select>
+
+                  <script>
+                    if (($('#id').html() == 2)) {
+                      $('#myselection').change(function() {
+                        if (($("#myselection option:selected").text() == "rouge") || ($("#myselection option:selected").text() == "Choisir une option")) {
+                          $('#myIMG').attr('src', "img/<?= $product['pic']; ?>");
+                        } else {
+                          $('#myIMG').attr('src', "img/surimiOrange.png");
+                        }
+                      })
+
+
+                    }
+                  </script>
+                </div>
+              </div>
+            </fieldset>
+
+            <div class="flex justify-center align-center my-5">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-7 h-7 fill-red-600 hidden mr-1" id="svg">
+                <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                <path d="M367.2 412.5L99.5 144.8C77.1 176.1 64 214.5 64 256c0 106 86 192 192 192c41.5 0 79.9-13.1 111.2-35.5zm45.3-45.3C434.9 335.9 448 297.5 448 256c0-106-86-192-192-192c-41.5 0-79.9 13.1-111.2 35.5L412.5 367.2zM512 256c0 141.4-114.6 256-256 256S0 397.4 0 256S114.6 0 256 0S512 114.6 512 256z" />
+              </svg><span id="trtm" class="text-xl font-bold text-red-600 capitalize"></span>
+            </div>
+
+            <div class="mt-8 flex">
+              <div>
+                <label for="quantity" class="sr-only">Qty</label>
+
+                <input type="number" id="quantity" min="1" value="1" class="w-12 rounded border-gray-200 py-3 text-center text-xs [-moz-appearance:_textfield] [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none" />
+              </div>
+
+              <button type="submit" class="ml-3 block capitalize rounded bg-blue-700 px-5 py-3 text-xs font-medium text-white hover:bg-blue-600">
+                Ajouter au panier
+              </button>
+            </div>
+          </form>
+        </div>
+      <?php endwhile ?>
+
+    </div>
+  </div>
+</section>
+
+<?php require_once('../inc/footer.php'); ?>
---
+++ fruitDeMer/inc/fetchData.php
@@ -0,0 +1,31 @@
+<?php
+include("../inc/db.php");
+$db = $conn;
+$tableName = "F_mer";
+$columns = ['id', 'name', 'pic', 'c1', 'c2', 'c3', 'c4', 'c5'];
+$fetchData = fetch_data($db, $tableName, $columns);
+function fetch_data($db, $tableName, $columns)
+{
+    if (empty($db)) {
+        $msg = "Database connection error";
+    } elseif (empty($columns) || !is_array($columns)) {
+        $msg = "columns Name must be defined in an indexed array";
+    } elseif (empty($tableName)) {
+        $msg = "Table Name is empty";
+    } else {
+        $columnName = implode(", ", $columns);
+        $query = "SELECT " . $columnName . " FROM $tableName" . " ORDER BY id DESC";
+        $result = $db->query($query);
+        if ($result == true) {
+            if ($result->num_rows > 0) {
+                $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                $msg = $row;
+            } else {
+                $msg = "No Data Found";
+            }
+        } else {
+            $msg = mysqli_error($db);
+        }
+    }
+    return $msg;
+}
