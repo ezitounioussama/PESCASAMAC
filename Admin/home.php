5b373d309efeadc7221472f9f8b3ed8df27c0679
ezitounioussama <zitounioussama95@gmail.com>
2022-12-13 22:09:03 -0500

    admin crud traitements work succesfully

--- Admin/home.php
+++ Admin/home.php
@@ -1,6 +1,6 @@
 <?php
-require_once('inc_Pcongele/header.php');
-require_once('inc_Pcongele/nav.php');
+require_once('inc/header.php');
+require_once('inc/nav.php');
 include "../inc/db.php";
 
 ?>
@@ -31,12 +31,26 @@ if (isset($_POST['submit'])) {
     $c1 = $_POST['c1'];
 
     $c2 = $_POST['c2'];
+    $c3 = $_POST['c3'];
+
+    $c4 = $_POST['c4'];
+    $c5 = $_POST['c5'];
+
 
     $t1 = $_POST['t1'];
     $t2 = $_POST['t2'];
+    $t3 = $_POST['t3'];
+    $t4 = $_POST['t4'];
+    $t5 = $_POST['t5'];
+    $t6 = $_POST['t6'];
 
-    $sql = "INSERT INTO `tbl_products`(`name`, `pic`, `c1`, `c2`, `t1` , `t2`) VALUES 
-    ('$name','$pic','$c1','$c2','$t1','$t2')";
+    $desc = $_POST['desc'];
+    $prc = $_POST['price'];
+
+    $cat = $_POST['cat'];
+
+    $sql = "INSERT INTO `tbl_products`(`name`, `pic`, `c1`, `c2`, `c3` ,`c4`, `c5`, `t1` , `t2`, `t3` , `t4`, `t5` , `t6`,`description`,`price`,`category`) VALUES 
+    ('$name','$pic','$c1','$c2','$c3','$c4','$c5','$t1','$t2','$t3','$t4','$t5','$t6','$desc','$prc','$cat')";
 
     $result = $conn->query($sql);
 
@@ -221,6 +235,32 @@ if (isset($_GET['id'])) {
                                                     </div>
                                                 </div>
 
+                                                <div class="row mb-4">
+                                                    <div class="col">
+                                                        <div class="form-outline">
+                                                            <input type="text" id="desc" name="desc" class="form-control" />
+                                                            <label class="form-label" for="desc">description</label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="form-outline">
+                                                            <input type="text" id="price" name="price" class="form-control" />
+                                                            <label class="form-label" for="price">price</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <label class="form-label" for="cat">category</label>
+                                                <select class="form-control" id="cat" name="cat">
+                                                    <option value="none" disabled>category</option>
+                                                    <option value="p_congele">p_congele</option>
+                                                    <option value="p_frait">p_frait</option>
+                                                    <option value="tarama">tarama</option>
+                                                    <option value="f_mer">f_mer</option>
+                                                    <option value="crustace">crustace</option>
+                                                    <option value="coquillage">coquillage</option>
+                                                    <option value="chairAraignee">chairAraignee</option>
+                                                    <option value="accompagnement">accompagnement</option>
+                                                </select>
                                                 <div class="modal-footer">
                                                     <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
                                                     <button type="submit" name="submit" class="btn btn-primary">ADD</button>
@@ -359,7 +399,7 @@ if (isset($_GET['id'])) {
                                                 <span class="text-secondary text-sm font-weight-normal"><?php echo $row['price']; ?></span>
                                             </td>
                                             <td class="align-middle">
-                                                <a href=' inc_Pcongele/update.php?id=<?php echo $row["id"]; ?>' class=" text-secondary font-weight-bold text-xs" data-bs-toggle="tooltip" data-bs-title="Edit data">
+                                                <a href=' inc/update.php?id=<?php echo $row["id"]; ?>' class=" text-secondary font-weight-bold text-xs" data-bs-toggle="tooltip" data-bs-title="Edit data">
                                                     <svg width="14" height="14" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M11.2201 2.02495C10.8292 1.63482 10.196 1.63545 9.80585 2.02636C9.41572 2.41727 9.41635 3.05044 9.80726 3.44057L11.2201 2.02495ZM12.5572 6.18502C12.9481 6.57516 13.5813 6.57453 13.9714 6.18362C14.3615 5.79271 14.3609 5.15954 13.97 4.7694L12.5572 6.18502ZM11.6803 1.56839L12.3867 2.2762L12.3867 2.27619L11.6803 1.56839ZM14.4302 4.31284L15.1367 5.02065L15.1367 5.02064L14.4302 4.31284ZM3.72198 15V16C3.98686 16 4.24091 15.8949 4.42839 15.7078L3.72198 15ZM0.999756 15H-0.000244141C-0.000244141 15.5523 0.447471 16 0.999756 16L0.999756 15ZM0.999756 12.2279L0.293346 11.5201C0.105383 11.7077 -0.000244141 11.9624 -0.000244141 12.2279H0.999756ZM9.80726 3.44057L12.5572 6.18502L13.97 4.7694L11.2201 2.02495L9.80726 3.44057ZM12.3867 2.27619C12.7557 1.90794 13.3549 1.90794 13.7238 2.27619L15.1367 0.860593C13.9869 -0.286864 12.1236 -0.286864 10.9739 0.860593L12.3867 2.27619ZM13.7238 2.27619C14.0917 2.64337 14.0917 3.23787 13.7238 3.60504L15.1367 5.02064C16.2875 3.8721 16.2875 2.00913 15.1367 0.860593L13.7238 2.27619ZM13.7238 3.60504L3.01557 14.2922L4.42839 15.7078L15.1367 5.02065L13.7238 3.60504ZM3.72198 14H0.999756V16H3.72198V14ZM1.99976 15V12.2279H-0.000244141V15H1.99976ZM1.70617 12.9357L12.3867 2.2762L10.9739 0.86059L0.293346 11.5201L1.70617 12.9357Z" fill="#64748B" />
                                                     </svg>
@@ -397,4 +437,4 @@ if (isset($_GET['id'])) {
 
 
 
-<?php require_once('inc_Pcongele/footer.php') ?>
+<?php require_once('inc/footer.php') ?>
