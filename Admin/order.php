0bfc6c4b65713ef69ead30e2c48a3e1c2224b05c
ezitounioussama <zitounioussama95@gmail.com>
2022-12-13 22:26:32 -0500

    admin order page still blank

--- Admin/order.php
+++ Admin/order.php
@@ -1,3 +1,9 @@
+<?php
+require_once('inc/header.php');
+
+require_once "../inc/db.php";
+?>
+
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 bg-slate-900 fixed-start " id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
@@ -8,10 +14,10 @@
     <div class="collapse navbar-collapse px-4  w-auto " id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link  active" href="./home.php">
+                <a class="nav-link " href="./home.php">
                     <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
                         <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                            <title>Products table</title>
+                            <title>Orders table</title>
                             <g id="dashboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                                 <g id="template" transform="translate(12.000000, 12.000000)" fill="#FFFFFF" fill-rule="nonzero">
                                     <path class="color-foreground" d="M0,1.71428571 C0,0.76752 0.76752,0 1.71428571,0 L22.2857143,0 C23.2325143,0 24,0.76752 24,1.71428571 L24,5.14285714 C24,6.08962286 23.2325143,6.85714286 22.2857143,6.85714286 L1.71428571,6.85714286 C0.76752,6.85714286 0,6.08962286 0,5.14285714 L0,1.71428571 Z" id="Path"></path>
@@ -27,10 +33,10 @@
 
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./order.php">
+                <a class="nav-link active" href="./order.php">
                     <div class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
                         <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                            <title>Products table</title>
+                            <title>Orders table</title>
                             <g id="dashboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                                 <g id="template" transform="translate(12.000000, 12.000000)" fill="#FFFFFF" fill-rule="nonzero">
                                     <path class="color-foreground" d="M0,1.71428571 C0,0.76752 0.76752,0 1.71428571,0 L22.2857143,0 C23.2325143,0 24,0.76752 24,1.71428571 L24,5.14285714 C24,6.08962286 23.2325143,6.85714286 22.2857143,6.85714286 L1.71428571,6.85714286 C0.76752,6.85714286 0,6.08962286 0,5.14285714 L0,1.71428571 Z" id="Path"></path>
@@ -54,7 +60,7 @@
         <div class="container-fluid py-1 px-2">
             <nav aria-label="breadcrumb">
 
-                <h6 class="font-weight-bold mb-0">Products table</h6>
+                <h6 class="font-weight-bold mb-0">Orders table</h6>
 
             </nav>
             <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
