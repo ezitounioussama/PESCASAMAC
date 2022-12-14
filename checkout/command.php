5b43588b1898c4a98d0dd203aca32c8b311f2175
ezitounioussama <zitounioussama95@gmail.com>
2022-12-13 21:44:09 -0500

    USER SIDE DONE
    M HAPPY :)

--- checkout/command.php
+++ checkout/command.php
@@ -1,4 +1,6 @@
 <?php
+require_once('../inc/header.php');
+require_once('../inc/nav.php');
 require_once('../inc/db.php');
 //  product data
 $productName = $_POST['product_name'];
@@ -34,7 +36,66 @@ if (isset($_POST['Command'])) {
     }
     if ($result == TRUE) {
 
-        echo "New record created successfully.";
+        mysqli_query($conn, "DELETE FROM cart");
+
+        if (isset($userLastName))
+            $sql = "SELECT first_name,GROUP_CONCAT(product_name separator '<br>')
+             AS products_list,GROUP_CONCAT(product_calibre separator '<br>') 
+             AS products_calibre ,GROUP_CONCAT(product_price separator '<br>') 
+             AS price ,
+             SUM(cast(product_price as float)) as ttl_price  FROM  tbl_orders WHERE last_name= '$userLastName'";
+
+
+        $result = $conn->query($sql); ?>
+        <table class="min-w-full divide-y-2 divide-gray-200 text-sm">
+            <thead>
+                <tr>
+                    <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                        first name
+                    </th>
+
+                    <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                        Product name
+                    </th>
+                    <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                        Calibre
+                    </th>
+                    <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                        Price
+                    </th>
+                    <th class="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900">
+                        Total
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody class="divide-y divide-gray-200">
+                <?php while ($row = $result->fetch_assoc()) {
+                    $nm = $row['first_name'];
+                    $p_name = $row['products_list'];
+                    $calibre = $row['products_calibre'];
+                    $prc = $row['price'];
+                    $Tprc = $row['ttl_price'];
+                }; ?>
+                <tr>
+                    <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                        <?php echo $nm ?>
+                    </td>
+                    <td class="whitespace-nowrap px-4 py-2 text-gray-700">
+                        <?php echo $p_name ?>
+                    </td>
+                    <td class="whitespace-nowrap px-4 py-2 text-gray-700">
+                        <?php echo $calibre ?>
+                    </td>
+                    <td class="whitespace-nowrap px-4 py-2 text-gray-700"><?php echo $prc ?></td>
+                    <td class="whitespace-nowrap px-4 py-2 text-gray-700"><?php echo $Tprc ?>DH</td>
+                </tr>
+            </tbody>
+        </table>
+<?php
+
+
+
     } else {
 
         echo "Error:" . $sql . "<br>" . $conn->error;
@@ -46,8 +107,7 @@ if (isset($_POST['Command'])) {
 
 
 
-
-
+require_once('../inc/footer.php');
 
 
 
